e0564f49f5401f55905e1ef65da30874d3f0877f
KravAD <dkrav@myuax.com>
2023-11-07 11:29:45 +0100

    prueba 4
    llamar void encontrar_triples_pitagoricos

--- main.cpp
+++ main.cpp
@@ -2,24 +2,26 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-void encontrar_triples_pitagoricos() {
-  for (int a; a < 20; a++){
-      cout << "a: " << a << endl;
-      cin >> a;
-      for (int b; b < 20; b++){
-            cout << "b: " << b << endl;
-            cin >> b;
-          for (int c; c < 20; c++){
-              if (sqrt(a^2 + b^2) == c){
-                  cout << a << " " << b << " " << c << endl;
-              }
-          }
-      }
-  }
+int a, b;
+void encontrar_triples_pitagoricos(){
+    for (int a; a < 20;){
+        for (int b; b < 20;){
+            for (int c; c < 20;){
+                if (sqrt(a^2 + b^2) == c){
+                    cout << a << " " << b << " " << c << endl;
+                }
+            }
+        }
+    }
 }
 
+
+
 int main() {
-    encontrar_triples_pitagoricos();
+    cout << "a: " << endl;
+    cin >> a;
+    cout << "b: " << endl;
+    cin >> b;
+       encontrar_triples_pitagoricos();
     return 0;
 }
