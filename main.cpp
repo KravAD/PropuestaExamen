7ecea2f19301d4513588cfacbb970c87e9da4bf5
KravAD <dkrav@myuax.com>
2023-11-07 11:21:55 +0100

    prueba 3
    cambio de solicitud de valores a y b

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 void encontrar_triples_pitagoricos() {
   for (int a; a < 20; a++){
+      cout << "a: " << a << endl;
+      cin >> a;
       for (int b; b < 20; b++){
+            cout << "b: " << b << endl;
+            cin >> b;
           for (int c; c < 20; c++){
               if (sqrt(a^2 + b^2) == c){
                   cout << a << " " << b << " " << c << endl;
